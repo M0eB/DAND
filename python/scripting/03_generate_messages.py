e2f00a7ad1fcb2e0179ac13970ff610c41a5bf1b
Mohamed Ismail <mohamed.t.ismail@gmail.com>
2018-05-28 06:15:45 -0400

    Initial Commit

---
+++ python/scripting/03_generate_messages.py
@@ -0,0 +1,19 @@
+names =  input("Enter student names: ").title().split(',')
+assignments =  input("Enter number of assignments: ").split(',')
+grades =  input("Enter grades: ").split(',')
+
+print(names)
+print(assignments)
+print(grades)
+
+
+# message string to be used for each student
+# HINT: use .format() with this string in your for loop
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+# write a for loop that iterates through each set of names, assignments, and grades to print each student's message
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
